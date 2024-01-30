122e3315b53aff894121bb678c84cd5b0ff4d67b
LaSteChips <121933446+LaSteChips@users.noreply.github.com>
2024-01-30 17:44:27 +0100

    update 1
    dans connexion.py :
    - liste des utilisateurs

--- connexion.py
+++ connexion.py
@@ -3,9 +3,10 @@ import tkinter as tk
 import sqlite3
 from tkinter import messagebox
 import subprocess
+from tabulate import tabulate
+import ttkthemes as ttk
 
-
-# DB
+# BDD
 conn = sqlite3.connect('user.db')
 cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS user (
@@ -14,12 +15,12 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS user (
                     password TEXT)''')
 conn.commit()
 
-# open the app if the connection is successful and close the login page
+# ouvre app.py si la connexion est réussie
 def open_app():
     window.destroy()    
     subprocess.run(["python", "app.py"])
 
-# check who is login
+# vérifier qui se connecte
 def check_user():
     username = entry_username.get()
     password = entry_password.get()
@@ -32,55 +33,91 @@ def check_user():
     else:
         messagebox.showerror("Erreur de connexion", "Identifiants incorrects")
 
-# user registration
+# enregistrement de l' utilisateur
 def save_user():
     username = entry_username.get()
     password = entry_password.get()
 
-    # check if the user already exists
+    # vérifier si l'utilisateur existe déjà
     cursor.execute('''SELECT * FROM user WHERE username = ?''', (username,))
     user_exist = cursor.fetchone()
 
     if user_exist:
         messagebox.showerror("Erreur d'enregistrement", "Ce nom d'utilisateur existe déjà.")
     else:
-        # save the user in the DB
+        # enregistrer l'utilisateur dans la BDD
         cursor.execute('''INSERT INTO user (username, password) VALUES (?, ?)''', (username, password))
         conn.commit()
         messagebox.showinfo("Enregistrement réussi", "Utilisateur enregistré avec succès !")
 
-# create a window
+def show_users():
+    # Connexion à la base de données SQLite
+    connexion = sqlite3.connect("user.db")
+    curseur = connexion.cursor()
+
+    # Récupération des utilisateurs depuis la base de données
+    curseur.execute("SELECT * FROM user")
+    utilisateurs = curseur.fetchall()
+
+    # Fermeture de la connexion à la base de données
+    connexion.close()
+
+    # Création de la fenêtre Tkinter
+    window = tk.Tk()
+    window.title("Liste des utilisateurs")
+
+    # paramètre de la fenêtre
+    window.geometry("200x100")
+    window.minsize(200, 100)
+    window.iconbitmap("images/logo delorme.ico")
+    window.configure(background='#36393e')
+
+    # Création d'une zone de texte pour afficher les utilisateurs
+    text_zone = tk.Text(window, fg='white', font='Arial')
+    text_zone.pack()
+    text_zone.configure(background='#36393e')
+
+    # Affichage des utilisateurs dans la zone de texte
+    for utilisateur in utilisateurs:
+        text_zone.insert(tk.END, f"ID: {utilisateur[0]}, Nom: {utilisateur[1]}\n")
+
+# créer la fenêtre d'application
 window = Tk()
 window.title("Page de connexion")
 
-# custom the window 
-window.geometry("240x140")
-window.minsize(320, 140)
+# paramètre de la fenêtre
+window.geometry("480x140")
+window.minsize(400, 140)
+window.maxsize(400, 140)
 window.iconbitmap("images/logo delorme.ico")
 window.configure(background='#36393e')
 
-# create a frame
+# créer une 'frame'
 frame = tk.Frame(window, bg='#36393e')
 frame.pack(padx=20, pady=20)
 
-# user settings
+# paramètre utilisateur
 label_username = tk.Label(frame, text="Nom d'Utilisateur:")
 label_username.grid(row=0, column=0, padx=5, pady=5)
 entry_username = tk.Entry(frame)
 entry_username.grid(row=0, column=1, padx=5, pady=5)
 
-# password settings
+# paramètres de mot de passe
 label_password = tk.Label(frame, text="Mot de passe:")
 label_password.grid(row=1, column=0, padx=5, pady=5)
 entry_password = tk.Entry(frame, show="*")
 entry_password.grid(row=1, column=1, padx=5, pady=5)
 
-# login button
-btn_register = tk.Button(frame, text="S'enregistrer", command=save_user)
+# bouton "d'enregistrer"
+btn_register = tk.Button(frame, text="S' Enregistrer", command=save_user)
 btn_register.grid(row=2, column=0, padx=5, pady=5)
 
-# connection button
-btn_connection = tk.Button(frame, text="Se connecter", command=check_user)
+# bouton "se connecter"
+btn_connection = tk.Button(frame, text="Se Connecter", command=check_user)
 btn_connection.grid(row=2, column=1, padx=5, pady=5)
+
+# bouton "liste d'utilisateurs"
+btn_connection = tk.Button(frame, text="Liste d'utilisateurs", command=show_users)
+btn_connection.grid(row=2, column=2, padx=5, pady=5)
 
 window.mainloop()
